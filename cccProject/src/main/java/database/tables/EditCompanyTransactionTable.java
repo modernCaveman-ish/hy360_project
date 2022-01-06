aaaf7f75e21264d529944908e07389bb06ccdb7b
modernCaveman-ish <stelios.z97@hotmail.com>
2022-01-06 18:57:23 +0200

    CompanyTransaction class needs test

--- cccProject/src/main/java/database/tables/EditCompanyTransactionTable.java
+++ cccProject/src/main/java/database/tables/EditCompanyTransactionTable.java
@@ -4,10 +4,109 @@
  */
 package database.tables;
 
+import connectDB.connectDB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mainClasses.Seller;
+
 /**
  *
  * @author stelios
  */
 public class EditCompanyTransactionTable {
+    connectDB connect = new connectDB();
     
+    public void insertCompanyTransaction(Seller seller) {
+        try {
+            Connection con = connect.connectionDB();
+            Statement stm = con.createStatement();
+            
+            String query = "INSERT INTO `seller`(`iban`, `name`, `debt`, `promithia`, `profit`) VALUES "
+                    + "('" + seller.getIban() + "','" + seller.getName() + "','" + seller.getDebt() + "','" + seller.getPromithia() + "','" + seller.getProfit() + "')";
+            System.out.println(query);
+            
+            stm.execute(query);
+            stm.close();
+            con.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Seller getSeller(String iban){
+        try {
+            Seller seller = new Seller();
+            
+            Connection con = connect.connectionDB();
+            Statement stm = con.createStatement();
+            
+            String query = "SELECT `iban`, `name`, `debt`, `promithia`, `profit` FROM `seller` WHERE iban = '" + iban + "'";
+            ResultSet rs = stm.executeQuery(query);
+            
+            while (rs.next()) {
+                seller.setUpFromResultSet(rs);
+            }
+            
+            stm.close();
+            con.close();
+            
+            return seller;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // error
+        return null;
+    }
+    
+    
+    public void updateSeller(Seller seller) {
+        try {
+            Connection con = connect.connectionDB();
+            Statement stm = con.createStatement();
+            
+            String query = "UPDATE `seller` SET `iban`='" + seller.getIban() + "',`name`='" + seller.getName() 
+                    + "',`debt`='" + seller.getDebt() + "',`promithia`='" + seller.getPromithia() + "',`profit`='" 
+                    + seller.getProfit() + "' WHERE iban = '" + seller.getIban() + "'";
+            
+            System.out.println("Edit PersonTable this is the update query: " + query);
+            
+            stm.execute(query);
+            
+            stm.close();
+            con.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void deleteSeller(String iban) {
+        try {
+            Connection con = connect.connectionDB();
+            Statement stm = con.createStatement();
+            
+            String query = "DELETE FROM `seller` WHERE iban = '" + iban +"' ";
+            
+            stm.execute(query);
+            stm.close();
+            con.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EditPersonTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- cccProject/src/main/java/mainClasses/CompanyTransaction.java
+++ cccProject/src/main/java/mainClasses/CompanyTransaction.java
@@ -4,10 +4,131 @@
  */
 package mainClasses;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author stelios
  */
 public class CompanyTransaction {
+    String id, iban_company, w_id, iban_seller;
+    enum Type { PISTWSH, XREWSH, PLHRWMI, EPISTROFI };
+    Type type = Type.PISTWSH; // default
+    String cost, tr_date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIban_company() {
+        return iban_company;
+    }
+
+    public void setIban_company(String iban_company) {
+        this.iban_company = iban_company;
+    }
+
+    public String getW_id() {
+        return w_id;
+    }
+
+    public void setW_id(String w_id) {
+        this.w_id = w_id;
+    }
+
+    public String getIban_seller() {
+        return iban_seller;
+    }
+
+    public void setIban_seller(String iban_seller) {
+        this.iban_seller = iban_seller;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getTr_date() {
+        return tr_date;
+    }
+
+    public void setTr_date(String tr_date) {
+        this.tr_date = tr_date;
+    }
     
+    // setUp seller from a given ResultSet
+    public void setUpFromResultSet(ResultSet rs) {
+        try {
+            this.id = rs.getString("id");
+            this.iban_company = rs.getString("iban_company");
+            this.w_id = rs.getString("w_id");
+            this.iban_seller = rs.getString("iban_seller");
+            this.cost = rs.getString("cost");
+            this.tr_date = rs.getString("tr_date");
+            //Thelei prosoxi
+            String tr_type = rs.getString("type");
+            switch(tr_type) {
+            case "PISTWSH":
+                this.type = Type.PISTWSH;
+                break;
+            case "XREWSH": 
+                this.type = Type.XREWSH;
+                break;
+            case "PLHRWMI": 
+                this.type = Type.PLHRWMI;
+                break;
+            case "EPISTROFI": 
+                this.type = Type.EPISTROFI;
+                break;    
+        }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Person.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public void setUpFromServletRequest(HttpServletRequest req){
+        this.iban_company = req.getParameter("iban_company");
+        this.iban_seller = req.getParameter("iban_seller");
+        this.id = req.getParameter("id");
+        this.w_id = req.getParameter("w_id");
+        this.cost = req.getParameter("cost");
+        this.tr_date = req.getParameter("tr_date");
+        // thelei prosoxi
+        String tr_type = req.getParameter("type");
+        switch(tr_type) {
+            case "PISTWSH":
+                this.type = Type.PISTWSH;
+                break;
+            case "XREWSH": 
+                this.type = Type.XREWSH;
+                break;
+            case "PLHRWMI": 
+                this.type = Type.PLHRWMI;
+                break;
+            case "EPISTROFI": 
+                this.type = Type.EPISTROFI;
+                break;    
+        }
+    }
 }
