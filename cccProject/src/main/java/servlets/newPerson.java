162d35406ad2ee0ef181eb1dfc93602951fbc0aa
haribunny <46565898+haribunny@users.noreply.github.com>
2022-01-14 18:03:11 +0200

    Update newPerson.java

--- cccProject/src/main/java/servlets/newPerson.java
+++ cccProject/src/main/java/servlets/newPerson.java
@@ -16,7 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mainClasses.Person;
 import database.tables.EditPersonTable;
 import java.sql.Connection;
@@ -41,7 +40,34 @@ public class newPerson extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, ParseException, SQLException, ClassNotFoundException {
 
+            connectDB connect = new connectDB();
+            Connection con = connect.connectionDB();
+            Statement st = con.createStatement();
+            
+            Person person = new Person();
+            
+            String exp_date_string = request.getParameter("exp_date");
+            Date exp_date = new SimpleDateFormat("yyyy-MM-dd").parse(exp_date_string);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            String date = formatter.format(exp_date);
+
+            person.setUpPerson(request);
+
+            System.out.println("NAME:" + person.getName());
+            
+            System.out.println("DATE:" + person.getExp_date().toString()) ;
+            
+            String query = "INSERT INTO `person` (`iban`, `name`, `balance`, `exp_date`, `debt_limit`, `debt`) "
+                    + "VALUES "
+                    + "('" + person.getIban() + "', "
+                    + "'" +  person.getName() + "', "
+                    + "'" +  person.getBalance() + "', "
+                    + "'" + date /* person.getExp_date() */ + "', "
+                    + "'" + person.getDebt_limit() + "', "
+                    + "'" +  person.getDebt() +"')";
         
+        st.execute(query);
+ 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -51,10 +77,12 @@ public class newPerson extends HttpServlet {
             out.println("<title>Servlet newPerson</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet newPerson at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet newPerson Successful!!info: " + person.toString() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
+        
+        st.close();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -91,44 +119,16 @@ public class newPerson extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            
-            try {
-            connectDB connect = new connectDB();
-            Connection con = connect.connectionDB();
-            Statement st = con.createStatement();
-            
-            Person person = new Person();
-            
-            String exp_date_string = request.getParameter("exp_date");
-            Date exp_date = new SimpleDateFormat("yyyy-MM-dd").parse(exp_date_string);
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-            String date = formatter.format(exp_date);
-
-            person.setUpPerson(request);
-
-            System.out.println("NAME:" + person.getName());
-            
-            System.out.println("DATE:" + person.getExp_date().toString()) ;
-            
-            String query = "INSERT INTO `person` (`iban`, `name`, `balance`, `exp_date`, `debt_limit`, `debt`) "
-                    + "VALUES "
-                    + "('" + person.getIban() + "', "
-                    + "'" +  person.getName() + "', "
-                    + "'" +  person.getBalance() + "', "
-                    + "'" + date /* person.getExp_date() */ + "', "
-                    + "'" + person.getDebt_limit() + "', "
-                    + "'" +  person.getDebt() +"')";
-        
-        st.execute(query);
-
-        st.close();
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(newPerson.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(newPerson.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(newPerson.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParseException ex) {
-            Logger.getLogger(newPerson.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
+  
     }
 
     /**
@@ -142,3 +142,4 @@ public class newPerson extends HttpServlet {
     }// </editor-fold>
 
 }
+
